75fd847832255d3ff051c70fcd920b63f405bdd9
DrBraunvieh <aar.schneider@web.de>
2024-10-18 16:43:18 +0200

    hi

--- SiebDesErastostenes.py
+++ SiebDesErastostenes.py
@@ -1,7 +1,9 @@
 import math
 
 zahlenListe = []
+primzahlen = []
 
+"""
 def sieb(liste, n):
     i = 2
     while(i <= math.ceil(math.sqrt(n))):
@@ -12,6 +14,20 @@ def sieb(liste, n):
         faktor = faktor + 1
         print(liste)
         i = i +1
+"""
+
+def siebRichtig(liste):
+    grenze = int(math.sqrt(len(liste)))
+    zahl = 2
+
+    while(zahl <= grenze):
+        faktor = 2
+
+        while(faktor*zahl < len(liste)):
+            liste.remove(faktor*zahl)
+            faktor = faktor + 1
+        zahl = zahl + 1
+    return liste
 
 def füllListe(m):
     i = 1
@@ -21,5 +37,5 @@ def füllListe(m):
 
 n = int(input("How many numbers shall the List contain?"))
 füllListe(n)
-print(zahlenListe)
-sieb(zahlenListe, n)
+primzahlen = siebRichtig(zahlenListe)
+print(primzahlen)
