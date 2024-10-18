d4c880f3e3d1efac493b88aaf191762bb454ed9e
DrBraunvieh <aar.schneider@web.de>
2024-10-18 16:49:23 +0200

    hi

--- SiebDesErastostenes.py
+++ SiebDesErastostenes.py
@@ -3,19 +3,6 @@ import math
 zahlenListe = []
 primzahlen = []
 
-"""
-def sieb(liste, n):
-    i = 2
-    while(i <= math.ceil(math.sqrt(n))):
-        faktor = 2
-        for x in liste:
-            if (faktor * x) % i == 0:
-                liste.remove(x)
-        faktor = faktor + 1
-        print(liste)
-        i = i +1
-"""
-
 def siebRichtig(liste):
     grenze = int(math.sqrt(len(liste)))
     zahl = 2
